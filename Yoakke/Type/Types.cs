21ea049590bef326c61add1b217519921456d632
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-05-27 10:37:14 +0200

    Documented the type-inference stuff

--- Yoakke/Type/Types.cs
+++ Yoakke/Type/Types.cs
@@ -6,22 +6,54 @@ using System.Text;
 
 namespace Yoakke.Type
 {
+    /// <summary>
+    /// The base class for types in the compiler.
+    /// </summary>
     abstract class Type
     {
+        /// <summary>
+        /// Constant for an empty <see cref="Type"/> list.
+        /// </summary>
         public static readonly IList<Type> EmptyList = new List<Type>();
 
+        /// <summary>
+        /// Unit type, used as no return type.
+        /// </summary>
         public static readonly Type Unit = Tuple(EmptyList);
+        /// <summary>
+        /// 32-bit signed integer type.
+        /// </summary>
         public static readonly Type I32 = Primitive("i32");
 
+        /// <summary>
+        /// Creates a <see cref="TypeVariable"/>.
+        /// </summary>
+        /// <returns>A new, unique <see cref="TypeVariable"/>.</returns>
         public static Type Variable() =>
             new TypeVariable();
 
+        /// <summary>
+        /// Creates a primitive type.
+        /// </summary>
+        /// <param name="name">The name of the primitive type.</param>
+        /// <returns>The <see cref="TypeConstructor"/> representing a primitive type.</returns>
         public static Type Primitive(string name) =>
             new TypeConstructor(name, EmptyList);
 
+        /// <summary>
+        /// Creates a tuple type.
+        /// </summary>
+        /// <param name="types">The list of <see cref="Type"/>s the tuple consists of.</param>
+        /// <returns>The <see cref="TypeConstructor"/> representing a tuple type.</returns>
         public static Type Tuple(IList<Type> types) =>
             new TypeConstructor("tuple", types);
 
+        /// <summary>
+        /// Creates a procedure type.
+        /// </summary>
+        /// <param name="parameters">The list of parameter <see cref="Type"/>s.</param>
+        /// <param name="ret">The return <see cref="Type"/>.</param>
+        /// <returns>The <see cref="TypeConstructor"/> representing a procedure type.</returns>
         public static Type Procedure(IList<Type> parameters, Type ret)
         {
             var types = parameters.ToList();
@@ -29,11 +61,22 @@ namespace Yoakke.Type
             return new TypeConstructor("procedure", types);
         }
 
+        /// <summary>
+        /// Returns the substitution for this <see cref="Type"/>. Could be itself, if there's no substitution.
+        /// </summary>
         public virtual Type Substitution => this;
 
+        /// <summary>
+        /// Checks, if this <see cref="Type"/> contains the given <see cref="TypeVariable"/>.
+        /// </summary>
+        /// <param name="typeVariable">The type variable to search for.</param>
+        /// <returns>True, if the <see cref="TypeVariable"/> is contained.</returns>
         public abstract bool Contains(TypeVariable typeVariable);
     }
 
+    /// <summary>
+    /// Represents a <see cref="Type"/> that's not inferred yet, and could be substituted for another.
+    /// </summary>
     class TypeVariable : Type
     {
         private Type? substitution;
@@ -51,6 +94,10 @@ namespace Yoakke.Type
         public override bool Contains(TypeVariable typeVariable) =>
             ReferenceEquals(this, typeVariable);
 
+        /// <summary>
+        /// Substitutes this type variable for another <see cref="Type"/>.
+        /// </summary>
+        /// <param name="type">The <see cref="Type"/> to substitute for.</param>
         public void SubstituteFor(Type type)
         {
             Debug.Assert(substitution == null, "Can only substitute for a type variable once!");
@@ -58,9 +105,18 @@ namespace Yoakke.Type
         }
     }
 
+    /// <summary>
+    /// Represents a concrete <see cref="Type"/> with a name and possible subtypes.
+    /// </summary>
     class TypeConstructor : Type
     {
+        /// <summary>
+        /// The name of this type.
+        /// </summary>
         public string Name { get; }
+        /// <summary>
+        /// The subtypes of this type.
+        /// </summary>
         public IList<Type> Subtypes { get; }
 
         internal TypeConstructor(string name, IList<Type> subtypes)
--- Yoakke/Type/Unifier.cs
+++ Yoakke/Type/Unifier.cs
@@ -4,30 +4,78 @@ using System.Text;
 
 namespace Yoakke.Type
 {
+    /// <summary>
+    /// Operations for type-inference and unification.
+    /// </summary>
     static class Unifier
     {
+        /// <summary>
+        /// Tries to unify the given types.
+        /// </summary>
+        /// <param name="t1">The first <see cref="Type"/> to unify.</param>
+        /// <param name="t2">The second <see cref="Type"/> to unify.</param>
         public static void Unify(Type t1, Type t2)
         {
+            void UnifyVarVar(TypeVariable v1, TypeVariable v2)
+            {
+                if (ReferenceEquals(v1, v2)) return;
+                v2.SubstituteFor(v1);
+            }
+
+            void UnifyCtorVar(TypeConstructor c1, TypeVariable v2)
+            {
+                if (c1.Contains(v2))
+                {
+                    throw new NotImplementedException("Type-recursion!");
+                }
+                v2.SubstituteFor(c1);
+            }
+
+            void UnifyCtorCtor(TypeConstructor c1, TypeConstructor c2)
+            {
+                if (c1.Name != c2.Name)
+                {
+                    throw new NotImplementedException("Type mismatch!");
+                }
+                if (c1.Subtypes.Count != c2.Subtypes.Count)
+                {
+                    throw new NotImplementedException("Subtype amount mismatch!");
+                }
+                for (int i = 0; i < c1.Subtypes.Count; ++i)
+                {
+                    Unify(c1.Subtypes[i], c2.Subtypes[i]);
+                }
+            }
+
             t1 = t1.Substitution;
             t2 = t2.Substitution;
             if (t1 is TypeVariable v1)
             {
                 if (t2 is TypeVariable v2)
                 {
-                    // Substitute type variable for another
-                    if (ReferenceEquals(v1, v2)) return;
-                    v2.SubstituteFor(v1);
+                    UnifyVarVar(v1, v2);
                     return;
                 }
-                // Substitute type variable for something else
-                if (t2.Contains(v1))
+                if (t2 is TypeConstructor c2)
                 {
-                    throw new NotImplementedException("Type-recursion!");
+                    UnifyCtorVar(c2, v1);
+                    return;
                 }
-                v1.SubstituteFor(t2);
-                return;
             }
-            // TODO
+            if (t1 is TypeConstructor c1)
+            {
+                if (t2 is TypeVariable v2)
+                {
+                    UnifyCtorVar(c1, v2);
+                    return;
+                }
+                if (t2 is TypeConstructor c2)
+                {
+                    UnifyCtorCtor(c1, c2);
+                    return;
+                }
+            }
+            throw new NotImplementedException();
         }
     }
 }
