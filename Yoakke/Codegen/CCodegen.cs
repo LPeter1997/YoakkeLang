3d3230418121104dc6e1513081697d01eb23abd9
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-06-08 16:09:56 +0200

    Made the C codegen compile externals

--- Yoakke/Codegen/CCodegen.cs
+++ Yoakke/Codegen/CCodegen.cs
@@ -13,6 +13,8 @@ namespace Yoakke.Backend
     class CCodegen : ICodegen
     {
         private StringBuilder builder = new StringBuilder();
+        private StringBuilder typeDeclarations = new StringBuilder();
+        private int typeCounter = 0;
 
         public string Compile(Assembly assembly)
         {
@@ -21,49 +23,62 @@ namespace Yoakke.Backend
 
         private string CompileAssembly(Assembly assembly)
         {
-            Write("#include <stdint.h>\n\n");
+            // We want the include to be on top
+            Write(typeDeclarations, "#include <stdint.h>\n\n");
+            foreach (var external in assembly.Externals)
+            {
+                DeclareExternal(external);
+                Write(builder, '\n');
+            }
+            Write(builder, '\n');
             foreach (var proc in assembly.Procedures)
             {
                 DeclareProc(proc);
-                Write('\n');
+                Write(builder, '\n');
             }
-            Write('\n');
+            Write(builder, '\n');
             foreach (var proc in assembly.Procedures)
             {
                 CompileProc(proc);
-                Write('\n');
+                Write(builder, '\n');
             }
-            return builder.ToString().Trim();
+            typeDeclarations.Append('\n');
+            return typeDeclarations.Append(builder).ToString().Trim();
+        }
+
+        private void DeclareExternal(Value.Extern external)
+        {
+            Write(builder, "extern ", external.Type, ' ', external.Name, ';');
         }
 
         private void DeclareProc(Proc proc)
         {
-            Write(proc.ReturnType, $" {proc.Name}(");
-            proc.Parameters.Intertwine(param => Write(param.Type), () => Write(", "));
-            Write(");");
+            Write(builder, proc.ReturnType, $" {proc.Name}(");
+            proc.Parameters.Intertwine(param => Write(builder, param.Type), () => Write(builder, ", "));
+            Write(builder, ");");
         }
 
         private void CompileProc(Proc proc)
         {
-            Write(proc.ReturnType, $" {proc.Name}(");
+            Write(builder, proc.ReturnType, $" {proc.Name}(");
             proc.Parameters.Intertwine(
-                param => Write(param.Type, ' ', param),
-                () => Write(", "));
-            Write(") {\n");
+                param => Write(builder, param.Type, ' ', param),
+                () => Write(builder, ", "));
+            Write(builder, ") {\n");
             foreach (var bb in proc.BasicBlocks)
             {
                 foreach (var ins in bb.Instructions) ForwardDeclareVariable(ins);
             }
             foreach (var bb in proc.BasicBlocks) CompileBasicBlock(bb);
-            Write("}\n");
+            Write(builder, "}\n");
         }
 
         private void CompileBasicBlock(BasicBlock basicBlock)
         {
-            Write(basicBlock.Name, ":\n");
+            Write(builder, basicBlock.Name, ":\n");
             foreach (var ins in basicBlock.Instructions)
             {
-                Write("    ", ins, ";\n");
+                Write(builder, "    ", ins, ";\n");
             }
         }
 
@@ -71,7 +86,7 @@ namespace Yoakke.Backend
         {
             if (instruction is ValueInstruction vi)
             {
-                Write("    ", vi.Value.Type, ' ', vi.Value, ";\n");
+                Write(builder, "    ", vi.Value.Type, ' ', vi.Value, ";\n");
             }
 
             switch (instruction)
@@ -84,91 +99,109 @@ namespace Yoakke.Backend
             case Instruction.Alloc alloc:
                 // T rX_value;
                 // T* rX = &rX_value;
-                Write("    ", alloc.ElementType, ' ', alloc.Value, "_value;\n");
+                Write(builder, "    ", alloc.ElementType, ' ', alloc.Value, "_value;\n");
                 break;
 
             default: throw new NotImplementedException();
             }
         }
 
-        private void CompileInstruction(Instruction instruction)
+        private void CompileInstruction(StringBuilder builder, Instruction instruction)
         {
             switch (instruction)
             {
             case Instruction.Alloc alloc:
-                Write(alloc.Value, " = &", alloc.Value, "_value");
+                Write(builder, alloc.Value, " = &", alloc.Value, "_value");
                 break;
 
             case Instruction.Ret ret:
-                Write("return");
-                if (ret.Value != null) Write(' ', ret.Value);
+                Write(builder, "return");
+                if (ret.Value != null) Write(builder, ' ', ret.Value);
                 break;
 
             case Instruction.Store store:
-                Write('*', store.Target, " = ", store.Value);
+                Write(builder, '*', store.Target, " = ", store.Value);
                 break;
 
             case Instruction.Load load:
-                Write(load.Value, " = *", load.Source);
+                Write(builder, load.Value, " = *", load.Source);
                 break;
 
             default: throw new NotImplementedException();
             }    
         }
 
-        private void CompileValue(Value value)
+        private void CompileValue(StringBuilder builder, Value value)
         {
             switch (value)
             {
             case Value.Register regVal:
-                Write('r', regVal.Index);
+                Write(builder, 'r', regVal.Index);
                 break;
 
             case Value.Int intVal:
-                Write(intVal.Value);
+                Write(builder, intVal.Value);
                 break;
 
             default: throw new NotImplementedException();
             }
         }
 
-        private void CompileType(Type type)
+        private void CompileType(StringBuilder builder, Type type)
         {
             switch (type)
             {
             case Type.Void _: 
-                Write("void");
+                Write(builder, "void");
                 break;
 
             case Type.Int intType:
-                if (!intType.Signed) Write('u');
-                Write($"int{intType.Bits}_t");
+                if (!intType.Signed) Write(builder, 'u');
+                Write(builder, $"int{intType.Bits}_t");
                 break;
 
             case Type.Ptr ptrType:
-                Write(ptrType.ElementType, '*');
+                Write(builder, ptrType.ElementType, '*');
                 break;
+
+            case Type.Proc procType:
+            {
+                // We declare it and just refer to it
+                // First declare return type and params, if any of them needs that
+                var returnTypeBuilder = new StringBuilder();
+                CompileType(returnTypeBuilder, procType.ReturnType);
+                var paramTypesBuilder = new StringBuilder();
+                procType.Parameters.Intertwine(
+                    param => Write(paramTypesBuilder, param),
+                    () => Write(paramTypesBuilder, ", "));
+                // Now create our new type
+                var typeName = $"func{typeCounter++}_t";
+                Write(typeDeclarations, "typedef ", returnTypeBuilder, $"({typeName})(", paramTypesBuilder ,");\n");
+                // Now write the built type's identifier to the correct place
+                Write(builder, typeName);
+            }
+            break;
 
             default: throw new NotImplementedException();
             }
         }
 
-        private void Write(params object[] args)
+        private void Write(StringBuilder builder, params object[] args)
         {
             foreach (var arg in args)
             {
                 switch (arg)
                 {
                 case Type t:
-                    CompileType(t);
+                    CompileType(builder, t);
                     break;
 
                 case Value v:
-                    CompileValue(v);
+                    CompileValue(builder, v);
                     break;
 
                 case Instruction i:
-                    CompileInstruction(i);
+                    CompileInstruction(builder, i);
                     break;
 
                 default:
