0daaf2417bcaa8785ab12c866bb6318ce4db5de3
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-08-27 13:18:10 +0200

    Update Span.cs

--- Yoakke.Text/Span.cs
+++ Yoakke.Text/Span.cs
@@ -47,10 +47,17 @@ namespace Yoakke.Text
         public static bool operator !=(Span s1, Span s2) => !(s1 == s2);
 
         /// <summary>
-        /// Checks, if a given <see cref="Position"/> is within the bounds of this <see cref="Span"/>.
+        /// Checks if a given <see cref="Position"/> is within the bounds of this <see cref="Span"/>.
         /// </summary>
         /// <param name="position">The <see cref="Position"/> to check.</param>
         /// <returns>True, if the <see cref="Position"/> is contained in this <see cref="Span"/>.</returns>
         public bool Contains(Position position) => Start <= position && position < End;
+
+        /// <summary>
+        /// Checks if this <see cref="Span"/> intersects with another one.
+        /// </summary>
+        /// <param name="other">The other <see cref="Span"/> to check intersection with.</param>
+        /// <returns>True, if the two <see cref="Span"/>s intersect.</returns>
+        public bool Intersects(Span other) => !(Start >= other.End || other.Start >= End);
     }
 }
