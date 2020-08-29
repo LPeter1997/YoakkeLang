2f3d458ed0a6847742dfe3ba24fa5e0cd8ae2dad
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-08-29 11:42:13 +0200

    Added a cursor and source builder

---
+++ Yoakke.Text/Cursor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yoakke.Text
+{
+    /// <summary>
+    /// Utility to keep track of the <see cref="Position"/> by feeding in characters from a source.
+    /// </summary>
+    public class Cursor
+    {
+        /// <summary>
+        /// The current <see cref="Position"/>.
+        /// </summary>
+        public Position Position { get; private set; }
+
+        private bool lastReturn;
+
+        /// <summary>
+        /// Appends the given character, moving the <see cref="Position"/> of this <see cref="Cursor"/>.
+        /// </summary>
+        /// <param name="c">The character to append.</param>
+        public void Append(char c)
+        {
+            if (c == '\r')
+            {
+                // Can be an OS-X 9 line-break
+                Position = Position.Newline();
+                lastReturn = true;
+            }
+            else if (c == '\n')
+            {
+                // if lastReturn, then it was a Windows \r\n, we already advanced a line for that
+                // Unix-style
+                if (!lastReturn) Position = Position.Newline();
+                lastReturn = false;
+            }
+            else
+            {
+                lastReturn = false;
+                // Only advance, if this is a visual character
+                if (!char.IsControl(c)) Position = Position.Advance();
+            }
+        }
+
+        /// <summary>
+        /// Appends the given string, moving the <see cref="Position"/> of this <see cref="Cursor"/>.
+        /// Basically appends each character of the string.
+        /// </summary>
+        /// <param name="str">The string to append.</param>
+        public void Append(string str)
+        {
+            foreach (var c in str) Append(c);
+        }
+
+        /// <summary>
+        /// Resets the <see cref="Position"/> to 0, 0.
+        /// </summary>
+        public void Reset() => Position = new Position();
+    }
+}
---
+++ Yoakke.Text/SourceBuilder.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yoakke.Text
+{
+    /// <summary>
+    /// A <see cref="StringBuilder"/>-like utility, that keeps track of the <see cref="Position"/>
+    /// while building the string.
+    /// </summary>
+    public class SourceBuilder
+    {
+        public int Capacity
+        {
+            get => builder.Capacity;
+            set => builder.Capacity = value;
+        }
+        public char this[int index]
+        {
+            get => builder[index];
+            set => builder[index] = value;
+        }
+        public int Length
+        {
+            get => builder.Length;
+            set => builder.Length = value;
+        }
+        public int MaxCapacity => builder.MaxCapacity;
+
+        /// <summary>
+        /// The current <see cref="Position"/> of the writing cursor.
+        /// </summary>
+        public Position Position => cursor.Position;
+
+        private Cursor cursor = new Cursor();
+        private StringBuilder builder = new StringBuilder();
+        private int cursorIndex;
+
+        public void Append(object? value)
+        {
+            builder.Append(value);
+            UpdateCursor();
+        }
+
+        public void AppendLine(string value = "")
+        {
+            builder.AppendLine(value);
+            UpdateCursor();
+        }
+
+        public void AppendJoin<T>(string separator, IEnumerable<T> enumerable)
+        {
+            builder.AppendJoin(separator, enumerable);
+            UpdateCursor();
+        }
+
+        public void AppendJoin<T>(char separator, IEnumerable<T> enumerable)
+        {
+            builder.AppendJoin(separator, enumerable);
+            UpdateCursor();
+        }
+
+        public void Clear()
+        {
+            builder.Clear();
+            cursor.Reset();
+        }
+
+        public override string ToString() => builder.ToString();
+
+        private void UpdateCursor()
+        {
+            for (; cursorIndex < builder.Length; ++cursorIndex)
+            {
+                cursor.Append(builder[cursorIndex]);
+            }
+        }
+    }
+}
