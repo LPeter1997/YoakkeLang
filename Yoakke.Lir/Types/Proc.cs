e72c2c515c3685a96853e56c5630a9e40d94df23
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-09-04 15:54:51 +0200

    Create Proc.cs

---
+++ Yoakke.Lir/Types/Proc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yoakke.DataStructures;
+
+namespace Yoakke.Lir.Types
+{
+    partial record Type
+    {
+        /// <summary>
+        /// Procedure type.
+        /// </summary>
+        public record Proc(CallConv CallConv, Type Return, IValueList<Type> Parameters) : Type
+        {
+            public override string ToString() => 
+                $"{Return} proc[callconv = {CallConv}]({string.Join(", ", Parameters)})";
+        }
+    }
+}
