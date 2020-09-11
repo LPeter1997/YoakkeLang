b846d55bc41f3ebc5eba77968e6ab1be24b71aa9
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-09-11 16:13:30 +0200

    Wrote a pointer value for the VM

---
+++ Yoakke.Lir/Runtime/PtrValue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yoakke.Lir.Types;
+using Yoakke.Lir.Values;
+
+namespace Yoakke.Lir.Runtime
+{
+    internal record PtrValue : Value
+    {
+        public Value? Value { get; set; }
+        public int Offset { get; set; }
+
+        public override Type Type { get; }
+
+        public PtrValue(Type type)
+        {
+            Type = type;
+        }
+
+        // TODO
+        public override string ToValueString() => "<some ptr>";
+    }
+}
