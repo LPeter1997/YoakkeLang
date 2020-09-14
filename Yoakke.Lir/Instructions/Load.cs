51bb0ea1f79cc5ec44391e84422d4acf664349fb
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-09-14 10:10:27 +0200

    Added load and store instruction objects

---
+++ Yoakke.Lir/Instructions/Load.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yoakke.Lir.Values;
+
+namespace Yoakke.Lir.Instructions
+{
+    partial class Instr
+    {
+        /// <summary>
+        /// Load value from an address instruction.
+        /// </summary>
+        public class Load : ValueInstr
+        {
+            /// <summary>
+            /// The address to load the <see cref="Value"/> from.
+            /// </summary>
+            public Value Address { get; set; }
+
+            public override IEnumerable<IInstrArg> InstrArgs
+            {
+                get
+                {
+                    yield return Result;
+                    yield return Address;
+                }
+            }
+
+            /// <summary>
+            /// Initializes a new <see cref="Load"/>.
+            /// </summary>
+            /// <param name="result">The <see cref="Register"/> to store the loaded value at.</param>
+            /// <param name="address">The address to load the <see cref="Value"/> from.</param>
+            public Load(Register result, Value address)
+                : base(result)
+            {
+                Address = address;
+            }
+
+            public override string ToString() => $"{Result} = load {Address}";
+        }
+    }
+}
---
+++ Yoakke.Lir/Instructions/Store.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yoakke.Lir.Values;
+
+namespace Yoakke.Lir.Instructions
+{
+    partial class Instr
+    {
+        /// <summary>
+        /// Store a value at an address instruction.
+        /// </summary>
+        public class Store : Instr
+        {
+            /// <summary>
+            /// The address to store the <see cref="Value"/> at.
+            /// </summary>
+            public Value Target { get; set; }
+            /// <summary>
+            /// The <see cref="Value"/> to store.
+            /// </summary>
+            public Value Value { get; set; }
+
+            public override IEnumerable<IInstrArg> InstrArgs
+            {
+                get
+                {
+                    yield return Target;
+                    yield return Value;
+                }
+            }
+
+            /// <summary>
+            /// Initializes a new <see cref="Store"/>.
+            /// </summary>
+            /// <param name="target">The address to store the value at.</param>
+            /// <param name="value">The <see cref="Value"/> to store.</param>
+            public Store(Value target, Value value)
+            {
+                Target = target;
+                Value = value;
+            }
+
+            public override string ToString() => $"store {Target}, {Value}";
+        }
+    }
+}
