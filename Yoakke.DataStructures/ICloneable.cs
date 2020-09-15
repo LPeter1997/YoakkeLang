f364016c63c58cb0a18833a4aa7db033f9614d20
Peter Lenkefi <lenkefi.peti@gmail.com>
2020-09-15 08:03:01 +0200

    Create ICloneable.cs

---
+++ Yoakke.DataStructures/ICloneable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yoakke.DataStructures
+{
+    /// <summary>
+    /// An interface for deep-copyable objects.
+    /// </summary>
+    /// <typeparam name="T">The type of the resulting clone.</typeparam>
+    public interface ICloneable<T>
+    {
+        /// <summary>
+        /// Clones the object.
+        /// </summary>
+        /// <returns>The cloned object.</returns>
+        public T Clone();
+    }
+}
