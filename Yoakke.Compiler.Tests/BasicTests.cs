fba6cb1a4ce613da583f8fc4b56251123edc20ad
Lenkefi Péter <lenkefi.peti@gmail.com>
2020-10-05 13:57:39 +0200

    Added a global tests

--- Yoakke.Compiler.Tests/BasicTests.cs
+++ Yoakke.Compiler.Tests/BasicTests.cs
@@ -18,5 +18,32 @@ const entry = proc() -> i32 {
 ";
             TestOnAllBackends<Func<Int32>>(src, Type.I32.NewValue(6243));
         }
+
+        [TestMethod]
+        public void ReturnGlobal()
+        {
+            string src = @"
+var x = 673;
+
+const entry = proc() -> i32 {
+    x
+};
+";
+            TestOnAllBackends<Func<Int32>>(src, Type.I32.NewValue(673));
+        }
+
+        [TestMethod]
+        public void RewriteGlobal()
+        {
+            string src = @"
+var x: i32;
+
+const entry = proc() -> i32 {
+    x = 9432;
+    x
+};
+";
+            TestOnAllBackends<Func<Int32>>(src, Type.I32.NewValue(9432));
+        }
     }
 }
