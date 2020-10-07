7005318314f23c126c4231de67f2ec3469127744
Lenkefi Péter <lenkefi.peti@gmail.com>
2020-10-07 21:41:56 +0200

    Wrote a CPP text reader to skip annoying bits

--- Yoakke.Compiler/Program.cs
+++ Yoakke.Compiler/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
+using Yoakke.C.Syntax.Cpp;
 using Yoakke.Compiler.Compile;
 using Yoakke.Compiler.Semantic;
 using Yoakke.DataStructures;
@@ -21,62 +23,16 @@ namespace Yoakke.Compiler
     {
         static void Main(string[] args)
         {
-            var srcPath = @"../../../../../samples/test.yk";
-            // Syntax
-            var src = File.ReadAllText(srcPath);
-            var srcFile = new SourceFile(srcPath, src);
-            var syntaxStatus = new SyntaxStatus();
-            var tokens = Lexer.Lex(srcFile, syntaxStatus);
-            var parser = new Parser(tokens, syntaxStatus);
-            var prg = parser.ParseFile();
-            var ast = ParseTreeToAst.Convert(prg);
-            ast = Desugaring.Desugar(ast);
-
-            //Console.WriteLine(prg.Dump());
-            Console.WriteLine(ast.Dump());
-
-            // Semantics
-            // TODO: Maye this should also be part of the dependency system?
-            // Probably yes!
-            var symTab = new SymbolTable();
-            symTab.GlobalScope.Define(new Symbol.Const("i32", new Value.User(Lir.Types.Type.I32)));
-            new DefineScope(symTab).Define(ast);
-            new DeclareSymbol(symTab).Declare(ast);
-            new ResolveSymbol(symTab).Resolve(ast);
-
-            // Compilation
-            //var system = new DependencySystem(symTab);
-            var codegen = new Codegen();
-            var buildStatus = new BuildStatus();
-            var asm = codegen.Generate(ast, buildStatus);
-            foreach (var err in buildStatus.Errors)
+            var src = @"
+hell\
+o world??(
+";
+            var reader = new CppTextReader(new StringReader(src));
+            while (true)
             {
-                Console.WriteLine(err.GetErrorMessage());
-            }
-            if (asm == null) return;
-
-            new CodePassSet().Pass(asm);
-            Console.WriteLine(asm);
-            Console.WriteLine("\n");
-            
-            // Run in the VM
-            var vm = new VirtualMachine(asm);
-            var result = vm.Execute("main", new Value[] { });
-            Console.WriteLine($"Result: {result}");
-
-            // Build an exe
-            var toolchain = Toolchains.All().First();
-            var build = new Build
-            {
-                CheckedAssembly = asm,
-                IntermediatesDirectory = "C:/TMP/program_build",
-                OutputPath = "C:/TMP/program.exe",
-            };
-            toolchain.Compile(build);
-
-            foreach (var err in build.Status.Errors)
-            {
-                Console.WriteLine(err.GetErrorMessage());
+                char? ch = reader.Next();
+                if (ch == null) break;
+                Console.Write(ch.Value);
             }
         }
     }
