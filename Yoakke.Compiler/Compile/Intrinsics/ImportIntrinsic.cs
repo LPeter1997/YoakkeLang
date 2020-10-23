d2cbe2d20153142fc23a4eb0e809c288c66d6631
Lenkefi Péter <lenkefi.peti@gmail.com>
2020-10-23 16:37:36 +0200

    Redid intrinsics a bit

---
+++ Yoakke.Compiler/Compile/Intrinsics/ImportIntrinsic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yoakke.DataStructures;
+using Yoakke.Lir.Types;
+using Yoakke.Lir.Values;
+using Yoakke.Syntax.Ast;
+using Type = Yoakke.Compiler.Semantic.Type;
+
+namespace Yoakke.Compiler.Compile.Intrinsics
+{
+    /// <summary>
+    /// An <see cref="Intrinsic"/> for importing other files.
+    /// </summary>
+    public class ImportIntrinsic : Intrinsic
+    {
+        public override Type Type { get; }
+
+        public ImportIntrinsic(IDependencySystem system)
+            : base(system)
+        {
+            var stringType = System.ReferToConstType("@c", "str");
+            Type = new Type.Proc(new ValueList<Type> { stringType }, Type.Type_);
+        }
+
+        public override Value Evaluate(IList<Expression> args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Yoakke.Compiler/Compile/Intrinsics/Intrinsic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yoakke.Compiler.Semantic;
+using Yoakke.Lir.Types;
+using Yoakke.Lir.Values;
+using Yoakke.Syntax.Ast;
+using Type = Yoakke.Compiler.Semantic.Type;
+
+namespace Yoakke.Compiler.Compile.Intrinsics
+{
+    /// <summary>
+    /// Base for compiler intrinsic values.
+    /// </summary>
+    public abstract class Intrinsic
+    {
+        /// <summary>
+        /// The <see cref="IDependencySystem"/>.
+        /// </summary>
+        public IDependencySystem System { get; set; }
+        /// <summary>
+        /// The semantic <see cref="Type"/> of this <see cref="Intrinsic"/>.
+        /// </summary>
+        public abstract Type Type { get; }
+
+        /// <summary>
+        /// Initiealizes a new <see cref="Intrinsic"/>.
+        /// </summary>
+        /// <param name="system">The <see cref="IDependencySystem"/>.</param>
+        public Intrinsic(IDependencySystem system)
+        {
+            System = system;
+        }
+
+        /// <summary>
+        /// Evaluates the intrinsic to a <see cref="Value"/>.
+        /// </summary>
+        /// <param name="args">The arguments passed in for the intrinsic.</param>
+        /// <returns>The <see cref="Value"/> the intrinsic evaluates to using the given arguments.</returns>
+        public abstract Value Evaluate(IList<Expression> args);
+    }
+}
