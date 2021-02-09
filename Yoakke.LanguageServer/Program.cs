5f9a0e319511989b4fac235b68219cf0269be25e
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-02-09 21:24:36 +0100

    Added a blank language server project

---
+++ Yoakke.LanguageServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using LangServer = OmniSharp.Extensions.LanguageServer;
+
+namespace Yoakke.LanguageServer
+{
+    class Program
+    {
+        static void Main(string[] args) => MainAsync(args).Wait();
+
+        private static async Task MainAsync(string[] args)
+        {
+            var server = await LangServer.Server.LanguageServer.From(options => options
+                .WithInput(Console.OpenStandardInput())
+                .WithOutput(Console.OpenStandardOutput())
+            );
+
+            await server.WaitForExit;
+        }
+    }
+}
